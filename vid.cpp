9bd160cc33bf9d01ea6b85926a525e1de7cb9133
TheEzo <willaschek.t@gmail.com>
2020-04-14 00:27:11 +0200

    asdf

--- vid.cpp
+++ vid.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <cmath>
 
 #define TAG 0
 #define NONUM -1
@@ -21,6 +22,7 @@ int main(int argc, char *argv[])
     int numprocs, myid, neighnumber, mynumber, number_count;
     numprocs = 4; // TODO
     int proc_cnt, first_val, index = 0, iterations, rest = 0;
+    double angle, tmp_angle, max_prev_angle = 0;
 
     MPI_Status stat;
     // inicializace MPI a ridicich hodnot
@@ -76,96 +78,25 @@ int main(int argc, char *argv[])
         proc_numbers.push_back(mynumber);
     }
 
+    for(int i = 0; i < iterations; i++){
+        angle = atan((proc_numbers[index] - first_val) / (myid )); // TODO x / index_prvku
+        MPI_Send(&angle, 1, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
 
+        if(myid == 0){
+            for(int j = 0; j < numprocs; j++){
+                MPI_Recv(&tmp_angle, 1, MPI_DOUBLE, j, TAG, MPI_COMM_WORLD, &stat);
+                if(tmp_angle > max_prev_angle){
+                    max_prev_angle = tmp_angle;
+                    cout << "v,";
+                }
+                else
+                    cout << "u,";
+            }
 
+        }
+        index++;
+    }
 
-//    // nacteni vstupniho souroru procesem "0" a rozeslani cisel jednotlivym procesum, vcetne "0"
-//    // vypis cisel ze souboru na stdout
-//    if(myid == 0){
-//        int number, receiver = 0;
-//        ifstream stream;
-//        stream.open("numbers", ios::in);
-//        while(stream.good()){
-//            number = stream.get();
-//            #ifndef TESTING
-//            if(receiver)
-//                cout << " ";
-//            #endif
-//            if(!stream.good())
-//                break;
-//            #ifndef TESTING
-//            cout << number;
-//            #endif
-//            MPI_Send(&number, 1, MPI_INT, receiver++, TAG, MPI_COMM_WORLD);
-//        }
-//        #ifndef TESTING
-//        cout << endl;
-//        #endif
-//        stream.close();
-//    }
-//
-//    MPI_Recv(&mynumber, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
-//
-//    int odd_max = 2 * (numprocs / 2) - 1;
-//    int even_max = 2 * ((numprocs - 1) / 2);
-//
-//    bool printed = false;
-//    double start;
-//    if(myid == 0)
-//        start = MPI_Wtime();
-//    // zacatek razeni, maximalne -- pocet_cisel / 2
-//    for(int k = 0; k < numprocs/2; k++){
-//
-//        // sude odeslou lichym a cekaji na vysledek
-//        // ve skutecnosti indexujeme procesory od "0", takze se vlastne jedna o liche procesory pri indexaci od "1"
-//        if(myid < odd_max && !(myid % 2)){
-//            MPI_Send(&mynumber, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD);
-//            MPI_Recv(&mynumber, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD, &stat);
-//        }
-//        else if(myid <= odd_max){ // liche porovnaji a vrati mensi
-//            MPI_Recv(&neighnumber, 1, MPI_INT, myid - 1, TAG, MPI_COMM_WORLD, &stat);
-//            if(mynumber < neighnumber){
-//                MPI_Send(&mynumber, 1, MPI_INT, myid - 1, TAG, MPI_COMM_WORLD);
-//                mynumber = neighnumber;
-//            }
-//            else
-//                MPI_Send(&neighnumber, 1, MPI_INT, myid - 1, TAG, MPI_COMM_WORLD);
-//        }
-//
-//        // liche odeslou sudym a cekaji na vysledek
-//        if(myid < even_max && myid % 2){
-//            MPI_Send(&mynumber, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD);
-//            MPI_Recv(&mynumber, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD, &stat);
-//        }
-//        else if(myid <= even_max && myid != 0){ // sude porovnaji a vrati mensi, 0 nic, protoze se posila na +1
-//            MPI_Recv(&neighnumber, 1, MPI_INT, myid - 1, TAG, MPI_COMM_WORLD, &stat);
-//            if(mynumber < neighnumber){
-//                MPI_Send(&mynumber, 1, MPI_INT, myid - 1, TAG, MPI_COMM_WORLD);
-//                mynumber = neighnumber;
-//            }
-//            else
-//                MPI_Send(&neighnumber, 1, MPI_INT, myid - 1, TAG, MPI_COMM_WORLD);
-//        }
-//    }
-//
-//
-//
-//    int *final = new int [numprocs];
-//    for(int i = 1; i < numprocs; i++){
-//        if(myid == i)
-//            MPI_Send(&mynumber, 1, MPI_INT, 0, TAG,  MPI_COMM_WORLD);
-//        if(myid == 0){
-//            MPI_Recv(&neighnumber, 1, MPI_INT, i, TAG, MPI_COMM_WORLD, &stat);
-//            final[i] = neighnumber;
-//        }
-//    }
-//    if(myid == 0){
-//        final[0] = mynumber;
-//        for(int i = 1; i < numprocs; i++){
-//            cout << final[i] << endl;
-//        }
-//    }
-//
     MPI_Finalize();
     return 0;
 }
