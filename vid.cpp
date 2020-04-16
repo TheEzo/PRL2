a2d10a8fe34f2b899d5ef28435ea5384b612b9c0
TheEzo <willaschek.t@gmail.com>
2020-04-17 00:06:08 +0200

    almost done

--- vid.cpp
+++ vid.cpp
@@ -19,16 +19,16 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int numprocs, myid, neighnumber, mynumber, number_count;
-    numprocs = 4; // TODO
+    int numprocs, myid, mynumber, number_count;
     int proc_cnt, first_val, index = 0, iterations, rest = 0;
-    double angle, tmp_angle, max_prev_angle = 0;
+    double angle;
 
     MPI_Status stat;
     // inicializace MPI a ridicich hodnot
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    vector<int> numbers;
 
     if(myid == 0){
         ifstream stream;
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
         string item;
         char delimiter = ',';
         stream.open("numbers", ios::in);
-        vector<int> numbers;
+
         stream >> item;
         ss.str(item);
         while(getline(ss, item, delimiter))
@@ -72,29 +72,68 @@ int main(int argc, char *argv[])
     // nastaveni postu iteraci pro prijem prvku
     MPI_Recv(&iterations, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
     // prijeti vsech potrebnych cisel
-    vector<int> proc_numbers;
+    vector<int> my_numbers;
     for(int i = 0; i < iterations; i++){
         MPI_Recv(&mynumber, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
-        proc_numbers.push_back(mynumber);
+        my_numbers.push_back(mynumber);
     }
 
+    // vypocet uhlu
+    vector<double> my_angles;
+    int num_index;
     for(int i = 0; i < iterations; i++){
-        angle = atan((proc_numbers[index] - first_val) / (myid )); // TODO x / index_prvku
-        MPI_Send(&angle, 1, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
+        num_index = i * numprocs + myid + 1; // index na vstupu
+        if(my_numbers[i] == NONUM)
+            my_angles.push_back(NONUM);
+        else
+            my_angles.push_back(atan((my_numbers[i] - first_val) / num_index));
+    }
 
-        if(myid == 0){
-            for(int j = 0; j < numprocs; j++){
-                MPI_Recv(&tmp_angle, 1, MPI_DOUBLE, j, TAG, MPI_COMM_WORLD, &stat);
-                if(tmp_angle > max_prev_angle){
-                    max_prev_angle = tmp_angle;
-                    cout << "v,";
-                }
-                else
-                    cout << "u,";
-            }
-
+    // ziskani vsech uhlu a sdileni sveho
+    double all_angles[iterations * numprocs];
+    for(int i = 0; i < iterations; i++){
+        // vsichni poslou vsem
+        for(int j = 0; j < numprocs; j++){
+            angle = my_angles[i];
+            MPI_Send(&angle, 1, MPI_DOUBLE, j, TAG, MPI_COMM_WORLD);
+            MPI_Recv(&angle, 1, MPI_DOUBLE, j, TAG, MPI_COMM_WORLD, &stat);
+            num_index = i * numprocs; // index v poli vsech uhlu
+            all_angles[num_index + j] = angle;
         }
-        index++;
+    }
+
+    // porovnani a zjisteni viditelnosti, odeslani vysledku
+    int state; // 1 = 'v', 0 = 'u', -1 = pro vyplneni poctu procesoru
+    for(int i = 0; i < iterations; i++){
+        bool visible = true;
+        num_index = i * numprocs; // index v poli vsech uhlu
+        if(all_angles[num_index] == NONUM)
+            state = -1;
+        else {
+            for (int j = num_index - 1; j >= 0 && visible; j++)
+                if (all_angles[num_index] <= all_angles[j])
+                    visible = false;
+            state = visible ? 1 : 0;
+        }
+        MPI_Send(&state, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
+    }
+
+    // sber a vypis dat
+    if(myid == 0){
+        cout << '_';
+        for(int i = 0; i < iterations; i++){
+            for(int j = 0; j < numprocs; j++){
+                num_index = i * numprocs + j;
+                cout << endl << num_index +1<< ": " << numbers[num_index + 1] << " : " << all_angles[num_index] << "\t";
+                MPI_Recv(&state, 1, MPI_INT, j, TAG, MPI_COMM_WORLD, &stat);
+                cout << state << " : ";
+                if(state == 1)
+                    cout << ",v";
+                else if(state == 0)
+                    cout << ",u";
+            }
+        }
+        cout << endl;
     }
 
     MPI_Finalize();
