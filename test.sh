ed065a1ebb607801050d492fbdea4be03ab85880
TheEzo <willaschek.t@gmail.com>
2020-04-19 00:42:14 +0200

    code done

--- test.sh
+++ test.sh
@@ -13,12 +13,14 @@ count=$(echo $numbers |tr "," "\n" |wc -l)
 echo $numbers > numbers
 
 mpic++ --prefix /usr/local/share/OpenMPI -o vid vid.cpp
-
+# TODO spocita cpu count
 if [ $count -le 20 ]
 then
-  mpirun --prefix /usr/local/share/OpenMPI -np $((count-1)) vid
+  mpirun --prefix /usr/local/share/OpenMPI -np 4 vid #$((count-1)) vid
+  mpirun --prefix /usr/local/share/OpenMPI -np 4 lin_vid #$((count-1)) lin_vid
 else
   mpirun --prefix /usr/local/share/OpenMPI -np 20 vid
+  mpirun --prefix /usr/local/share/OpenMPI -np 20 lin_vid
 fi
 
-rm -f numbers vid
+#rm -f numbers vid
--- vid.cpp
+++ vid.cpp
@@ -16,10 +16,11 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     int numprocs, myid, mynumber, first_val, iterations;
-    double angle;
+    double angle, max_angle;
+    vector<int> my_numbers;
 
     MPI_Status stat;
     // inicializace MPI a ridicich hodnot
@@ -54,17 +55,27 @@ int main(int argc, char *argv[])
         for(int i = 0; i < numprocs; i++)
             MPI_Send(&proc_cnt, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
 
-        // posilani cisel jednotlivym procesorum, preskoceni prvniho, to neni treba
-        for(int i = 0; i < number_count; i++){
-            rest = i % numprocs;
-            mynumber = numbers[i+1];
-            MPI_Send(&mynumber, 1, MPI_INT, rest, TAG, MPI_COMM_WORLD);
+        // posilani cisel jednotlivym procesorum, preskoceni prvniho cisla, to neni treba
+        rest = numprocs * proc_cnt;
+        int index;
+        for(int i = 0; i < numprocs; i++){
+            for(int j = 0; j < proc_cnt && rest; j++){
+                index = proc_cnt * i + j + 1;
+                if(index < numbers.size())
+                    mynumber = numbers[proc_cnt * i + j + 1];
+                else
+                    break;
+                MPI_Send(&mynumber, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
+                rest--;
+            }
         }
         // zbytek vyplnime vatou
-        for(int i = rest+1; i < numprocs; i++){
-            mynumber = NONUM;
-            MPI_Send(&mynumber, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-        }
+        for(int j = numprocs-1; j >= 0; j--)
+            for(int i = rest; i < proc_cnt && rest; i++){
+                mynumber = NONUM;
+                MPI_Send(&mynumber, 1, MPI_INT, numprocs-1, TAG, MPI_COMM_WORLD);
+                rest--;
+            }
     }
 
     // nastaveni pocatecniho bodu
@@ -72,67 +83,90 @@ int main(int argc, char *argv[])
     // nastaveni postu iteraci pro prijem prvku
     MPI_Recv(&iterations, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
     // prijeti vsech potrebnych cisel
-    vector<int> my_numbers;
     for(int i = 0; i < iterations; i++){
         MPI_Recv(&mynumber, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
         my_numbers.push_back(mynumber);
     }
 
-    // vypocet uhlu prirazenych procesoru
+    // vypocet uhlu cisel prirazenych procesoru
     vector<double> my_angles;
     int num_index;
     for(int i = 0; i < iterations; i++){
-        num_index = i * numprocs + myid + 1; // index na vstupu
+        num_index = iterations * myid + i + 1; // index na vstupu
         if(my_numbers[i] == NONUM)
             my_angles.push_back(NONUM);
         else
             my_angles.push_back(atan((my_numbers[i] - first_val) / (double)num_index));
+        max_angle = i ? max(my_angles[i], my_angles[i-1]) : my_angles[i];
     }
 
-    // porovnani uhlu s max_prev_angle, odeslani a vypsani
-    int proc;
-    int state; // 1 = 'v', 0 = 'u', -1 = pro vyplneni poctu procesoru
-    if(myid == 0)
-        cout << "_";
-    for(int i = 0; i < iterations; i++){
-        num_index = numprocs * i;
-        for(int j = 0; j < numprocs; j++){
-            // kontrola, zda cislo nalezi procesoru
-            proc = (num_index + j) % numprocs;
-            if(proc == myid){
-                // 1. cislo je vzdy videt
-                if(num_index + j == 0){
-                    angle = my_angles[i];
-                    state = 1;
-                    MPI_Send(&state, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
-                }
-                // ostatni prijmou uhel a spocitaji viditelnost
-                else{
-                    MPI_Recv(&angle, 1, MPI_DOUBLE, (num_index + j - 1) % numprocs, TAG, MPI_COMM_WORLD, &stat);
-                    if(my_numbers[i] == NONUM)
-                        state = -1;
-                    else
-                        state = my_angles[i] > angle ? 1 : 0;
-                    MPI_Send(&state, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
-                    angle = max(my_angles[i], angle);
-                }
-                // preposlani max_prev_angle uhlu dalsimu procesoru
-                if(num_index + j < iterations * numprocs + 1){
-                    MPI_Send(&angle, 1, MPI_DOUBLE, (proc + 1) % numprocs, TAG, MPI_COMM_WORLD);
-                }
+    vector<double> node_angles = {max_angle};
+    int size;
+    // UpSweep
+    for(int d = 2; d <= numprocs; d = d*2){
+        for(int i = numprocs; i > 0; i -= d){
+            if(myid == i-d/2-1){
+                MPI_Send(&max_angle, 1, MPI_DOUBLE, i-1, TAG, MPI_COMM_WORLD);
             }
-            // sber a vypis dat
-            if(myid == 0){
-                MPI_Recv(&state, 1, MPI_INT, j, TAG, MPI_COMM_WORLD, &stat);
-                if(state == 1)
-                    cout << ",v";
-                else if(state == 0)
-                    cout << ",u";
+            if(myid == i-1){
+                MPI_Recv(&angle, 1, MPI_DOUBLE, i-d/2-1, TAG, MPI_COMM_WORLD, &stat);
+                max_angle = max(node_angles[node_angles.size() - 1], angle);
+                node_angles.push_back(angle);
             }
         }
     }
-    if(myid == 0)
+
+    // Nahrazeni posledni prvku za neutralni
+    if(myid == numprocs - 1){
+        node_angles.push_back(0);
+    }
+    // DownSweep
+    for(int d = numprocs; d >= 2; d = d/2){
+        for(int i = numprocs; i > 0; i -= d){
+            if(myid == i-d/2-1){
+                MPI_Recv(&angle, 1, MPI_DOUBLE, i-1, TAG, MPI_COMM_WORLD, &stat);
+                node_angles.push_back(angle);
+            }
+            if(myid == i-1){
+                size = node_angles.size() - 1;
+                angle = node_angles[size];
+                MPI_Send(&angle, 1, MPI_DOUBLE, i-d/2-1, TAG, MPI_COMM_WORLD);
+                node_angles.pop_back();
+                max_angle = max(angle, node_angles[size-1]);
+                node_angles.pop_back();
+                node_angles.push_back(max_angle);
+            }
+        }
+    }
+    // sekvencni porovnani prvku jendoho procesoru
+    max_angle = node_angles[1];
+    int res;
+    for(int i = 0; i < iterations; i++){
+        if(my_numbers[i] == NONUM){
+            res = NONUM;
+        }
+        else{
+            if(my_angles[i] > max_angle){
+                res = 1;
+                max_angle = my_angles[i];
+            }
+            else
+                res = 0;
+        }
+        MPI_Send(&res, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
+    }
+
+    if(myid == 0){
+        cout << "_";
+        for(int i = 0; i < numprocs; i++){
+            for(int j = 0; j < iterations; j++){
+                MPI_Recv(&res, 1, MPI_INT, i, TAG, MPI_COMM_WORLD, &stat);
+                if(res != NONUM)
+                    cout << (res ? ",v" : ",u");
+            }
+        }
         cout << endl;
+    }
 
     MPI_Finalize();
     return 0;
